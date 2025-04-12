41b3ea82682deb7f0c802ec3d1bde068587d3c37
Rogueone122 <107857608+Rogueone122@users.noreply.github.com>
2025-04-12 16:51:32 +0200

    Update submit-data.js

--- netlify/functions/submit-data.js
+++ netlify/functions/submit-data.js
@@ -1,14 +1,10 @@
-// netlify/functions/submit-data.js
 exports.handler = async function(event, context) {
-  // Les données que tu veux envoyer à l'ESP32
-  const data = {
-    pseudo: "MMMMMMMMMM",
-    pdp: 3,
-    version: "v0.1"
-  };
-
   return {
     statusCode: 200,
-    body: JSON.stringify(data)
+    body: JSON.stringify({
+      pseudo: "Rogue",
+      pdp: 3,
+      version: "v0.1"
+    })
   };
 };
