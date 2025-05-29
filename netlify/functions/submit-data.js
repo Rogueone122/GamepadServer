5aa56706b458162764b9d3a36d6b2e020731be64
Rogueone122 <107857608+Rogueone122@users.noreply.github.com>
2025-05-29 14:09:53 +0200

    Update submit-data.js

--- netlify/functions/submit-data.js
+++ netlify/functions/submit-data.js
@@ -4,7 +4,7 @@ exports.handler = async function(event, context) {
     body: JSON.stringify({
       version: "v0.1",
       importance: "Facultative", // tu peux mettre "mineure", "critique", etc.
-      date_de_sortie: "26-05-25",
+      date_de_sortie: "29-05-25",
       commentaire: "Test"
     })
   };
