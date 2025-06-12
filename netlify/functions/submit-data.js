962f6f1499515ec88485f88429763a481f68401f
Rogueone122 <107857608+Rogueone122@users.noreply.github.com>
2025-06-12 23:44:50 +0200

    Update submit-data.js

--- netlify/functions/submit-data.js
+++ netlify/functions/submit-data.js
@@ -2,7 +2,7 @@ exports.handler = async function(event, context) {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      version: "v0.1.4",
+      version: "v0.1.5",
       importance: "Facultative", // Facultative | Majeure | Urgente
       date_de_sortie: "12-06-25",
       commentaire: "Test"
